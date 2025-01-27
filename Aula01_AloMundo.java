048ba029c421d45df2a231b28a3a895993cac814
Pedro dos Santos Borges <56660097+peuborg@users.noreply.github.com>
2025-01-27 20:25:51 -0300

    aula 01

---
+++ Aula01_AloMundo.java
@@ -0,0 +1,5 @@
+public class Aula01_AloMundo {
+    public static void main(String[] args) {
+        System.out.println("Alô Mundo");
+    }
+}
---
+++ Aula01_Diamante.java
@@ -0,0 +1,5 @@
+public class Aula01_Diamante {
+    public static void main(String[] args) {
+        System.out.println("*");
+    }
+}
